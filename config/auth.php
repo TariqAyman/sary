b76fd0df26200d4c4abfdce43087d5660c928f3a
Tariq Ayman <tariq.ayman94@gmail.com>
2021-09-08 00:06:11 +0200

    config auth for api

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -36,8 +36,8 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
+        'api' => [
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
@@ -88,7 +88,7 @@ return [
 
     'passwords' => [
         'users' => [
-            'provider' => 'users',
+            'provider' => 'api',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
