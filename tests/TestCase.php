921fb678836233f65560c7b956a9b5ea36c2c51e
Tariq Ayman <tariq.ayman94@gmail.com>
2021-09-08 00:14:10 +0200

    install api token package & config test env

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,40 @@
 
 namespace Tests;
 
+use Exception;
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+    use RefreshDatabase, WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('migrate:refresh');
+        Artisan::call('db:seed');
+    }
+
+    public function __get($key)
+    {
+        if ($key === 'faker') return $this->faker;
+        throw new Exception('Unknown Key Requested');
+    }
+
+    protected function authenticate()
+    {
+        $user = ['email' => 'admin@admin.com', 'password' => 'password'];
+
+        return Auth::attempt($user);
+    }
+
 }
